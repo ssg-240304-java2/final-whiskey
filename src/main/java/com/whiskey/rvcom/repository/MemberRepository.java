ccb362b59f21920ac20a2e84f777a2156cae5116
Jaehyeok Song <speter650@gmail.com>
2024-08-10 16:40:10 +0900

    feat(report): 회원 기능 구현을 위한 엔티티-DTO간 매핑 레포지토리 생성 #83
    pr날립니다

---
+++ src/main/java/com/whiskey/rvcom/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.whiskey.rvcom.repository;
+
+import com.whiskey.rvcom.entity.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
+
