4a70c5fad7f3cd4d76ba76439dc6df375a77c112
l2chmnl <l2chmnl@gmail.com>
2024-08-12 11:07:32 +0900

    feat(Report): 식당신고목록 전체조회 메서드  및 기능 구현 시 필요한 DTO 생성

--- src/main/java/com/whiskey/rvcom/config/AppConfig.java
+++ src/main/java/com/whiskey/rvcom/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.whiskey.rvcom.config;
 
+
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
