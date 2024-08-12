b57396315b562f0372f7ba1341a3f4b41dae3353
Awesome <awesome_devnet@outlook.com>
2024-08-12 11:32:22 +0900

    feat: 리뷰댓글 관련 서비스 보충 작성

--- src/main/java/com/whiskey/rvcom/review/ReviewCommentService.java
+++ src/main/java/com/whiskey/rvcom/review/ReviewCommentService.java
@@ -13,7 +13,23 @@ public class ReviewCommentService {
     private final ReviewRepository reviewRepository;
     private final ReviewCommentRepository reviewCommentRepository;
 
+    /**
+     * 특정 리뷰에 대응하는 댓글 리스트 조회
+     * @param review 리뷰 객체
+     * @return 댓글 리스트
+     */
     public List<ReviewComment> getCommentsForReview(Review review) {
         return reviewCommentRepository.findByReview(review);
+    }
+
+    // 리뷰댓글의 id로 리뷰댓글 객체 조회
+
+    /**
+     * 리뷰댓글의 id로 리뷰댓글 객체 조회
+     * @param commentId 리뷰댓글 id
+     * @return 리뷰댓글 객체
+     */
+    public ReviewComment getCommentById(Long commentId) {
+        return reviewCommentRepository.findById(commentId).orElseThrow(IllegalArgumentException::new);
     }
 }
--- src/main/java/com/whiskey/rvcom/review/ReviewService.java
+++ src/main/java/com/whiskey/rvcom/review/ReviewService.java
@@ -1,10 +1,13 @@
 package com.whiskey.rvcom.review;
 
+import com.whiskey.rvcom.entity.member.Member;
 import com.whiskey.rvcom.entity.review.Review;
 import com.whiskey.rvcom.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +23,15 @@ public class ReviewService {
      */
     public Review getReviewById(Long reviewId) {
         return reviewRepository.findById(reviewId).orElseThrow(IllegalArgumentException::new);
+    }
+
+    /**
+     * 회원 객체로 리뷰 리스트 조회
+     * @param member 회원 객체
+     * @return 리뷰 리스트
+     */
+    public List<Review> getReviewsByMember(Member member) {
+        return reviewRepository.findByMember(member);
     }
 
     // TODO. Resource 담당자쪽 service에 구현되도록 업무요청 전달 -> 컨트롤러 모듈에서 받아 처리할 예정
