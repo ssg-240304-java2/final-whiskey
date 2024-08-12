fd7e7c5fbe46f0121e53976afd5b0edd308db24a
Jaehyeok Song <162393172+speter6501@users.noreply.github.com>
2024-08-12 09:36:21 +0900

    Error: repository위치 변경 #83
    repository 현재위치가 잘못되서 고칩니다

--- src/main/java/com/whiskey/rvcom/repository/MemberRepository.java
+++ src/main/java/com/whiskey/rvcom/repository/MemberRepository.java
@@ -1,8 +1,7 @@
-package repository;
+package com.whiskey.rvcom.repository;
 
 import com.whiskey.rvcom.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
