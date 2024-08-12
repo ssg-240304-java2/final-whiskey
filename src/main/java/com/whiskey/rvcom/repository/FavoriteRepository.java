6af7e9022242281215c6889b6def2b994933c58c
kebin <kebin0591@gmail.com>
2024-08-12 11:05:56 +0900

    feat(favorite): FavoriteRepository에 새로운 쿼리 메소드 추가
    - FavoriteRepository.java:
      - 특정 회원의 즐겨찾기 목록을 조회하는 메소드 추가
      - 특정 회원의 특정 음식점 즐겨찾기 여부를 확인하는 메소드 추가
      - 특정 회원의 특정 음식점 즐겨찾기를 삭제하는 메소드 추가

--- src/main/java/com/whiskey/rvcom/repository/FavoriteRepository.java
+++ src/main/java/com/whiskey/rvcom/repository/FavoriteRepository.java
@@ -3,6 +3,16 @@ package com.whiskey.rvcom.repository;
 import com.whiskey.rvcom.entity.favorite.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FavoriteRepository extends JpaRepository<Favorite , Long> {
+import java.util.List;
 
+public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+
+    // 특정 회원의 즐겨찾기 목록 조회
+    List<Favorite> findByMemberId(Long memberId);
+
+    // 특정 회원의 특정 음식점 즐겨찾기 여부 확인
+    boolean existsByMemberIdAndRestaurantId(Long memberId, Long restaurantId);
+
+    // 특정 회원의 특정 음식점 즐겨찾기 삭제
+    void deleteByMemberIdAndRestaurantId(Long memberId, Long restaurantId);
 }
