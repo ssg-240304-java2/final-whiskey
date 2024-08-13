ebcd977294177915d1029a8dac6c7266b60f23ed
l2chmnl <l2chmnl@gmail.com>
2024-08-13 12:10:31 +0900

    feat(report): 리뷰 댓글 신고 등록 기능 구현 완료 #133  - test 코드 및 최소한의 단정문 완료입니다.

--- src/test/java/com/whiskey/rvcom/report/ReviewCommentReportTest.java
+++ src/test/java/com/whiskey/rvcom/report/ReviewCommentReportTest.java
@@ -37,4 +37,22 @@ public class ReviewCommentReportTest {
         Assertions.assertNotNull(reports);
 
     }
+
+
+    @Test
+    @DisplayName("리뷰댓글신고등록")
+    public void insertReviewCommentReport() {
+
+        ReviewCommentDTO reviewCommentDTO = new ReviewCommentDTO(3L, false, LocalDateTime.now(), 3, 5, "여기 너무 좋아요! 호구당하기! 좋네요!", Rating.FIVE_STAR);
+        ReviewCommentReportDTO reviewCommentReportDTO = new ReviewCommentReportDTO(null, false, true, LocalDateTime.now(), reviewCommentDTO, "댓글 신고 제목3", "댓글 신고 내용3");
+
+        reviewCommentReportService.saveReviewCommentReport(reviewCommentReportDTO);
+
+        // 조회 성공 후 목록 가져오기
+        List<ReviewCommentReportDTO> list = reviewCommentReportService.getAllReviewCommentReports();
+
+//      현재 테스트 코드로 등록한 신고와 DB에 등록된 신고의 내용이 같은지 비교
+        Assertions.assertEquals(list.get(list.size()-1).getContent(), reviewCommentReportDTO.getContent());
+    }
+
 }
