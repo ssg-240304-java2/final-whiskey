b0d9f29f8ff6732b51349d77ebed2a5f8f059ee8
youngrok <yr951103@gmail.com>
2024-08-14 17:04:41 +0900

    feat: 음식점 공지사항 조회 및 작성 기능 구현 (#169, #170)
    - 공지사항 조회, 작성 로직 구현
    - RestaurantNotice 클래스에 생성자 추가
    - 요청 및 응답 객체 담을 DTO 클래스 생성
    - RestaurantNoticeRepository에 메서드 추가
    - 테스트 코드 작성

---
+++ src/main/java/com/whiskey/rvcom/notice/RestaurantNoticeService.java
@@ -0,0 +1,37 @@
+package com.whiskey.rvcom.notice;
+
+import com.whiskey.rvcom.entity.restaurant.Restaurant;
+import com.whiskey.rvcom.entity.restaurant.RestaurantNotice;
+import com.whiskey.rvcom.notice.dto.RestaurantNoticeRequestDTO;
+import com.whiskey.rvcom.notice.dto.RestaurantNoticeResponseDTO;
+import com.whiskey.rvcom.repository.RestaurantNoticeRepository;
+import com.whiskey.rvcom.repository.RestaurantRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class RestaurantNoticeService {
+    private final RestaurantRepository restaurantRepository;
+    private final RestaurantNoticeRepository noticeRepository;
+
+    // TODO: 해당 음식점의 공지사항 전체 조회
+    public List<RestaurantNoticeResponseDTO> findNoticeByRestaurantId(Long restaurantId) {
+        return noticeRepository.findByRestaurantId(restaurantId).stream()
+                .map(it -> new RestaurantNoticeResponseDTO(
+                        it.getTitle(),
+                        it.getContent(),
+                        it.getCreatedAt()
+                )).toList();
+    }
+
+    // TODO: 해당 음식점의 점주가 공지사항 작성
+    public void save(RestaurantNoticeRequestDTO request, Long restaurantId) {
+        Restaurant restaurant = restaurantRepository.getReferenceById(restaurantId);
+        noticeRepository.save(new RestaurantNotice(request.title(), request.content(), restaurant));
+    }
+
+    // TODO: 해당 음식점의 점주가 공지사항 삭제
+}
