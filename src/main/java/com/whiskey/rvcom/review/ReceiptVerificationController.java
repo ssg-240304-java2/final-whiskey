2b9bb4390070b81939a3c3cb523a7b6abe3af187
Awesome <awesome_devnet@outlook.com>
2024-08-20 11:01:50 +0900

    feat: 영수증인증 페이지 요청 핸들러 위치변경

---
+++ src/main/java/com/whiskey/rvcom/review/ReceiptVerificationController.java
@@ -0,0 +1,14 @@
+package com.whiskey.rvcom.review;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/receipt")
+public class ReceiptVerificationController {
+        @GetMapping("/receipt-verification")
+        // 영수증 인증 페이지로 이동
+        public String getReceiptVerification() {
+            return "receiptVerification";
+        }
+}
