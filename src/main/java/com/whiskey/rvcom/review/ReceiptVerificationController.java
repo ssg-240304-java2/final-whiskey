644567746c2f23457ea560a0530b7a1854ca2721
Awesome <awesome_devnet@outlook.com>
2024-08-20 15:52:54 +0900

    format: 단순 코드 내 탭 문자 조정

--- src/main/java/com/whiskey/rvcom/review/ReceiptVerificationController.java
+++ src/main/java/com/whiskey/rvcom/review/ReceiptVerificationController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/receipt")
 public class ReceiptVerificationController {
-        @GetMapping("/receipt-verification")
-        // 영수증 인증 페이지로 이동
-        public String getReceiptVerification() {
-            return "receiptVerification";
-        }
+    @GetMapping("/receipt-verification")
+    // 영수증 인증 페이지로 이동
+    public String getReceiptVerification() {
+        return "receiptVerification";
+    }
 }
