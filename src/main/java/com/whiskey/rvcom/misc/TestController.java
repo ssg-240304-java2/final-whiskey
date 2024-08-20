6b81a64bdb108b3135354c584b3c8098cafc490a
Lucas-Choi-17 <csissis@naver.com>
2024-08-20 21:25:40 +0900

    feat: 음식점 상세 페이지에서 음식점의 정보를 받아와 표시 #144
    - 이름
    - 카테고리
    - 금일 영업시간 및 영업 여부
    - 식당 전화번호, 주소

--- src/main/java/com/whiskey/rvcom/misc/TestController.java
+++ src/main/java/com/whiskey/rvcom/misc/TestController.java
@@ -1,12 +1,26 @@
 package com.whiskey.rvcom.misc;
 
+import com.whiskey.rvcom.entity.restaurant.OpenCloseTime;
+import com.whiskey.rvcom.entity.restaurant.Restaurant;
+import com.whiskey.rvcom.restaurant.service.RestaurantService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Controller
+@Slf4j
 public class TestController {
+
+    RestaurantService restaurantService;
+
+    public TestController(RestaurantService restaurantService) {
+        this.restaurantService = restaurantService;
+    }
 
     @GetMapping("/")
     // 테스트 메인 페이지로 이동(로컬호스트 홈)
@@ -49,13 +63,13 @@ public class TestController {
     public String getReceiptVerification() {
         return "receiptVerification";
     }
-    
+
     @GetMapping("/write-review")
     // 리뷰 작성 페이지로 이동
     public String getWriteReview() {
         return "writeReview";
     }
-    
+
     @GetMapping("/search-results")
     // 검색 결과 페이지로 이동
     public String searchResults() {
@@ -73,7 +87,7 @@ public class TestController {
     public String getOwnerDashboard() {
         return "owner/dashboard";
     }
-    
+
     @GetMapping("/adminMain")
     // 관리자-신고 페이지로 이동
     public String getAdminReport() {
@@ -86,9 +100,63 @@ public class TestController {
     }
 
     @GetMapping("/restaurant/{restaurantId}/{tab}")
-    public String getRestaurantDetailWithTab(@PathVariable Long restaurantId, @PathVariable String tab) {
+    public String getRestaurantDetailWithTab(Model model, @PathVariable Long restaurantId, @PathVariable String tab) {
         // TODO: restaurantId와 tab에 따른 데이터 로딩 로직 구현
         // TODO: 모델에 restaurantId와 tab 정보 추가
+        Restaurant restaurant = restaurantService.getRestaurantById(restaurantId);
+
+        model.addAttribute("restaurant", restaurant);
+
+        String today = LocalDate.now().getDayOfWeek().toString();
+
+        switch (today) {
+            case "MONDAY":
+                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getMonday(), model);
+                break;
+            case "TUESDAY":
+                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getTuesday(), model);
+                break;
+            case "WEDNESDAY":
+                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getWednesday(), model);
+                break;
+            case "THURSDAY":
+                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getThursday(), model);
+                break;
+            case "FRIDAY":
+                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getFriday(), model);
+                break;
+            case "SATURDAY":
+                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getSaturday(), model);
+                break;
+            case "SUNDAY":
+                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getSunday(), model);
+                break;
+        }
         return "restaurantDetail";
+
+    }
+
+    private void restaurantTimeData(OpenCloseTime openCloseTime, Model model) {
+        String hours;
+        if (openCloseTime == null) {
+            model.addAttribute("status", "미영업");
+            hours = "";
+        } else if (openCloseTime.isOpen()) {
+            String openTime = openCloseTime.getOpenTime();
+            String closeTime = openCloseTime.getCloseTime();
+            LocalTime open = LocalTime.parse(openTime);
+            LocalTime close = LocalTime.parse(closeTime);
+            LocalTime now = LocalTime.now();
+            if (now.isBefore(open) || now.isAfter(close)) {
+                model.addAttribute("status", "영업종료");
+            } else {
+                model.addAttribute("status", "영업중");
+            }
+            hours = openCloseTime.getOpenTime() + " - " + openCloseTime.getCloseTime();
+        } else {
+            model.addAttribute("status", "미영업");
+            hours = "";
+        }
+        model.addAttribute("hours", hours);
     }
 }
