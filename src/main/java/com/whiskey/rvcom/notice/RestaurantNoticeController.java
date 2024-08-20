91715f79dac7e5d6af8c1ce631d23b5cfe7cfb0e
youngrok <yr951103@gmail.com>
2024-08-20 23:36:29 +0900

    feat: 경로 변수로 restaurantId를 받도록 API 수정
    - @GetMapping 경로에서 쿼리 파라미터 방식 대신 경로 변수 방식으로 변경
    - 공지사항 조회 시 restaurantId를 경로에서 받아 처리하도록 변경

--- src/main/java/com/whiskey/rvcom/notice/RestaurantNoticeController.java
+++ src/main/java/com/whiskey/rvcom/notice/RestaurantNoticeController.java
@@ -4,10 +4,7 @@ import com.whiskey.rvcom.notice.dto.RestaurantNoticeRequestDTO;
 import com.whiskey.rvcom.notice.dto.RestaurantNoticeResponseDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,8 +15,8 @@ public class RestaurantNoticeController {
     private final RestaurantNoticeService noticeService;
 
     // TODO: 해당 음식점의 공지사항 전체 조회
-    @GetMapping("/restaurant/notice")
-    public List<RestaurantNoticeResponseDTO> findNoticeByRestaurantId(Long restaurantId) {
+    @GetMapping("/restaurant/{restaurantId}/notice")
+    public List<RestaurantNoticeResponseDTO> findNoticeByRestaurantId(@PathVariable Long restaurantId) {
         return noticeService.findNoticeByRestaurantId(restaurantId);
     }
 
