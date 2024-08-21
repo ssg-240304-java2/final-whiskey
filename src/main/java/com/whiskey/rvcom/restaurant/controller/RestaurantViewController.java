6099bf29af759f17894708db866a15b52b3d65ef
Lucas-Choi-17 <csissis@naver.com>
2024-08-21 20:23:37 +0900

    feat: 위치기반 검색 페이지에서 상세 정보 페이지 요청 url 변경에 따라 매핑 컨트롤러 주석 처리 #144

--- src/main/java/com/whiskey/rvcom/restaurant/controller/RestaurantViewController.java
+++ src/main/java/com/whiskey/rvcom/restaurant/controller/RestaurantViewController.java
@@ -2,6 +2,7 @@ package com.whiskey.rvcom.restaurant.controller;
 
 import com.whiskey.rvcom.entity.restaurant.OpenCloseTime;
 import com.whiskey.rvcom.entity.restaurant.Restaurant;
+import com.whiskey.rvcom.entity.restaurant.WeeklyOpenCloseTime;
 import com.whiskey.rvcom.restaurant.service.RestaurantService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -30,62 +31,121 @@ public class RestaurantViewController {
         return restaurantService.getNearbyRestaurantByLocation(lat, lng);
     }
 
-    @GetMapping("/detail/{restaurantId}")
-    public String restaurantDetail(Model model, @PathVariable long restaurantId) {
-
-        Restaurant restaurant = restaurantService.getRestaurantById(restaurantId);
-
-        model.addAttribute("restaurant", restaurant);
-
-        String today = LocalDate.now().getDayOfWeek().toString();
-
-        switch (today) {
-            case "MONDAY":
-                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getMonday(), model);
-                break;
-            case "TUESDAY":
-                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getTuesday(), model);
-                break;
-            case "WEDNESDAY":
-                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getWednesday(), model);
-                break;
-            case "THURSDAY":
-                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getThursday(), model);
-                break;
-            case "FRIDAY":
-                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getFriday(), model);
-                break;
-            case "SATURDAY":
-                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getSaturday(), model);
-                break;
-            case "SUNDAY":
-                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getSunday(), model);
-                break;
-        }
-        return "restaurantDetail";
-    }
-
-    private void restaurantTimeData(OpenCloseTime openCloseTime, Model model) {
-        String hours;
-        if (openCloseTime == null) {
-            model.addAttribute("status", "미영업");
-            hours = "";
-        } else if (openCloseTime.isOpen()) {
-            String openTime = openCloseTime.getOpenTime();
-            String closeTime = openCloseTime.getCloseTime();
-            LocalTime open = LocalTime.parse(openTime);
-            LocalTime close = LocalTime.parse(closeTime);
-            LocalTime now = LocalTime.now();
-            if (now.isBefore(open) || now.isAfter(close)) {
-                model.addAttribute("status", "영업종료");
-            } else {
-                model.addAttribute("status", "영업중");
-            }
-            hours = openCloseTime.getOpenTime() + " - " + openCloseTime.getCloseTime();
-        } else {
-            model.addAttribute("status", "미영업");
-            hours = "";
-        }
-        model.addAttribute("hours", hours);
-    }
+    // 레스토랑 상세 페이지로 이동하는 매핑은 testController에 있음
+//    @GetMapping("/detail/{restaurantId}")
+//    public String restaurantDetail(Model model, @PathVariable long restaurantId) {
+//
+//        Restaurant restaurant = restaurantService.getRestaurantById(restaurantId);
+//
+//        model.addAttribute("restaurant", restaurant);
+//
+//        String today = LocalDate.now().getDayOfWeek().toString();
+//
+//        switch (today) {
+//            case "MONDAY":
+//                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getMonday(), model);
+//                break;
+//            case "TUESDAY":
+//                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getTuesday(), model);
+//                break;
+//            case "WEDNESDAY":
+//                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getWednesday(), model);
+//                break;
+//            case "THURSDAY":
+//                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getThursday(), model);
+//                break;
+//            case "FRIDAY":
+//                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getFriday(), model);
+//                break;
+//            case "SATURDAY":
+//                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getSaturday(), model);
+//                break;
+//            case "SUNDAY":
+//                restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getSunday(), model);
+//                break;
+//        }
+//
+//        restaurantWeeklyOpeningTime(model, restaurant.getWeeklyOpenCloseTime());
+//
+//        return "restaurantDetail";
+//    }
+//
+//    private void restaurantTimeData(OpenCloseTime openCloseTime, Model model) {
+//        String hours;
+//        if (openCloseTime == null) {
+//            model.addAttribute("status", "미영업");
+//            hours = "";
+//        } else if (openCloseTime.isOpen()) {
+//            String openTime = openCloseTime.getOpenTime();
+//            String closeTime = openCloseTime.getCloseTime();
+//            LocalTime open = LocalTime.parse(openTime);
+//            LocalTime close = LocalTime.parse(closeTime);
+//            LocalTime now = LocalTime.now();
+//            if (now.isBefore(open) || now.isAfter(close)) {
+//                model.addAttribute("status", "영업종료");
+//            } else {
+//                model.addAttribute("status", "영업중");
+//            }
+//            hours = openCloseTime.getOpenTime() + " - " + openCloseTime.getCloseTime();
+//        } else {
+//            model.addAttribute("status", "미영업");
+//            hours = "";
+//        }
+//        model.addAttribute("hours", hours);
+//    }
+//
+//    private void restaurantWeeklyOpeningTime(Model model, WeeklyOpenCloseTime weeklyOpenCloseTime) {
+//        if (weeklyOpenCloseTime.getMonday() != null) {
+//            model.addAttribute("monday", convert12HourFormat(weeklyOpenCloseTime.getMonday().getOpenTime()) + " - " + convert12HourFormat(weeklyOpenCloseTime.getMonday().getCloseTime()));
+//        } else {
+//            model.addAttribute("monday", "미영업");
+//        }
+//
+//        if (weeklyOpenCloseTime.getTuesday() != null) {
+//            model.addAttribute("tuesday", convert12HourFormat(weeklyOpenCloseTime.getTuesday().getOpenTime()) + " - " + convert12HourFormat(weeklyOpenCloseTime.getTuesday().getCloseTime()));
+//        } else {
+//            model.addAttribute("tuesday", "미영업");
+//        }
+//
+//        if (weeklyOpenCloseTime.getWednesday() != null) {
+//            model.addAttribute("wednesday", convert12HourFormat(weeklyOpenCloseTime.getWednesday().getOpenTime()) + " - " + convert12HourFormat(weeklyOpenCloseTime.getWednesday().getCloseTime()));
+//        } else {
+//            model.addAttribute("wednesday", "미영업");
+//        }
+//
+//        if (weeklyOpenCloseTime.getThursday() != null) {
+//            model.addAttribute("thursday", convert12HourFormat(weeklyOpenCloseTime.getThursday().getOpenTime()) + " - " + convert12HourFormat(weeklyOpenCloseTime.getThursday().getCloseTime()));
+//        } else {
+//            model.addAttribute("thursday", "미영업");
+//        }
+//
+//        if (weeklyOpenCloseTime.getFriday() != null) {
+//            model.addAttribute("friday", convert12HourFormat(weeklyOpenCloseTime.getFriday().getOpenTime()) + " - " + convert12HourFormat(weeklyOpenCloseTime.getFriday().getCloseTime()));
+//        } else {
+//            model.addAttribute("friday", "미영업");
+//        }
+//
+//        if (weeklyOpenCloseTime.getSaturday() != null) {
+//            model.addAttribute("saturday", convert12HourFormat(weeklyOpenCloseTime.getSaturday().getOpenTime()) + " - " + convert12HourFormat(weeklyOpenCloseTime.getSaturday().getCloseTime()));
+//        } else {
+//            model.addAttribute("saturday", "미영업");
+//        }
+//
+//        if (weeklyOpenCloseTime.getSunday() != null) {
+//            model.addAttribute("sunday", convert12HourFormat(weeklyOpenCloseTime.getSunday().getOpenTime()) + " - " + convert12HourFormat(weeklyOpenCloseTime.getSunday().getCloseTime()));
+//        } else {
+//            model.addAttribute("sunday", "미영업");
+//        }
+//    }
+//
+//    private String convert12HourFormat(String time) {
+//        int hour = Integer.parseInt(time.split(":")[0]);
+//        String minute = time.split(":")[1];
+//
+//        if (hour >= 12) {
+//            return hour + ":" + minute + " PM";
+//        } else {
+//            return hour + ":" + minute + " AM";
+//        }
+//    }
 }
