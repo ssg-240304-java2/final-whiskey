1788d1efcca78ba452fe6caae827b730546faa0c
Lucas-Choi-17 <csissis@naver.com>
2024-08-22 19:40:33 +0900

    feat: 음식점 검색 페이지 매핑 메서드 TestController -> RestaurantViewController로 이동

--- src/main/java/com/whiskey/rvcom/restaurant/controller/RestaurantViewController.java
+++ src/main/java/com/whiskey/rvcom/restaurant/controller/RestaurantViewController.java
@@ -5,6 +5,7 @@ import com.whiskey.rvcom.restaurant.service.RestaurantService;
 import com.whiskey.rvcom.review.ReviewService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,5 +25,12 @@ public class RestaurantViewController {
     @ResponseBody
     public List<RestaurantCardDTO> userLocationMain(@RequestParam double lat, @RequestParam double lng) {
         return restaurantService.getNearbyRestaurantByLocation(lat, lng);
+    }
+
+    @GetMapping("/search-results")
+    // 검색 결과 페이지로 이동
+    public String searchResults(@RequestParam(value = "searchText", required = false) String searchText, Model model) {
+        model.addAttribute("searchText", searchText);
+        return "searchResults";
     }
 }
