e886bb94eee4553431dd5b175729a8dc9dc66295
Lucas-Choi-17 <csissis@naver.com>
2024-08-22 14:59:55 +0900

    fix: controller의 기능 일부를 service로 이동 #260

--- src/main/java/com/whiskey/rvcom/restaurant/controller/RestaurantViewController.java
+++ src/main/java/com/whiskey/rvcom/restaurant/controller/RestaurantViewController.java
@@ -1,15 +1,11 @@
 package com.whiskey.rvcom.restaurant.controller;
 
-import com.whiskey.rvcom.entity.restaurant.OpenCloseTime;
-import com.whiskey.rvcom.entity.restaurant.Restaurant;
 import com.whiskey.rvcom.restaurant.dto.RestaurantCardDTO;
 import com.whiskey.rvcom.restaurant.service.RestaurantService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -26,73 +22,6 @@ public class RestaurantViewController {
     @GetMapping("/userLocationMain")
     @ResponseBody
     public List<RestaurantCardDTO> userLocationMain(@RequestParam double lat, @RequestParam double lng) {
-        List<Restaurant> allByDistance = restaurantService.getNearbyRestaurantByLocation(lat, lng);
-
-        List<RestaurantCardDTO> restaurantCardList = new ArrayList<>();
-        for (Restaurant restaurant : allByDistance) {
-            restaurantCardList.add(convertToDTO(restaurant, lat, lng));
-        }
-
-        return restaurantCardList;
-    }
-
-    private RestaurantCardDTO convertToDTO(Restaurant restaurant, double latitude, double longitude) {
-
-        double distance = 6371 * Math.acos(Math.cos(Math.toRadians(latitude)) * Math.cos(Math.toRadians(restaurant.getAddress().getLatitude())) * Math.cos(Math.toRadians(restaurant.getAddress().getLongitude()) - Math.toRadians(longitude)) + Math.sin(Math.toRadians(latitude)) * Math.sin(Math.toRadians(restaurant.getAddress().getLatitude())));
-        distance = Math.round(distance * 100) / 100.0;
-
-        String distanceString;
-        if (distance > 1) {
-            distanceString = distance + "km";
-        } else {
-            distanceString = distance * 1000 + "m";
-        }
-
-        String today = LocalDate.now().getDayOfWeek().toString();
-
-        String openingHour = null;
-        switch (today) {
-            case "MONDAY":
-                openingHour = restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getMonday());
-                break;
-            case "TUESDAY":
-                openingHour = restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getTuesday());
-                break;
-            case "WEDNESDAY":
-                openingHour = restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getWednesday());
-                break;
-            case "THURSDAY":
-                openingHour = restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getThursday());
-                break;
-            case "FRIDAY":
-                openingHour = restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getFriday());
-                break;
-            case "SATURDAY":
-                openingHour = restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getSaturday());
-                break;
-            case "SUNDAY":
-                openingHour = restaurantTimeData(restaurant.getWeeklyOpenCloseTime().getSunday());
-                break;
-        }
-
-        RestaurantCardDTO restaurantCardDTO = new RestaurantCardDTO(
-                restaurant.getId(),
-                restaurant.getName(),
-                restaurant.getCategory().name(),
-                distanceString,
-                openingHour
-        );
-
-        return restaurantCardDTO;
-    }
-
-    private String restaurantTimeData(OpenCloseTime openCloseTime) {
-        String hours;
-        if (openCloseTime == null) {
-            hours = "휴무";
-        } else {
-            hours = openCloseTime.getOpenTime() + " - " + openCloseTime.getCloseTime();
-        }
-        return hours;
+        return restaurantService.getNearbyRestaurantByLocation(lat, lng);
     }
 }
