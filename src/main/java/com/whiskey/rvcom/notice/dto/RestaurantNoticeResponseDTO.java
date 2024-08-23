3139e5d8c1a66c16f7a8ca6a144d4e7c739a4802
youngrok <yr951103@gmail.com>
2024-08-24 00:12:18 +0900

    refactor:  DTO에 id와 삭제여부 필드 추가 및 매핑 업데이트
    - 공지사항 식별을 위한 id 필드와 삭제 여부를 확인하기 위해 isDeleted 필드 추가
    - 필드 변경으로 인한 객체값도 변경하여 매핑
    - 추가로 사용자페이지에서 공지사항 작성 버튼 안보이도록 설정

--- src/main/java/com/whiskey/rvcom/notice/dto/RestaurantNoticeResponseDTO.java
+++ src/main/java/com/whiskey/rvcom/notice/dto/RestaurantNoticeResponseDTO.java
@@ -3,8 +3,10 @@ package com.whiskey.rvcom.notice.dto;
 import java.time.LocalDateTime;
 
 public record RestaurantNoticeResponseDTO(
+        Long id,
         String title,
         String content,
+        Boolean isDeleted,
         LocalDateTime createdAt
 ) {
 }
