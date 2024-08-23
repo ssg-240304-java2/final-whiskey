c3fa9ae9940566352a0d3694619124745b9cf55f
Lucas-Choi-17 <csissis@naver.com>
2024-08-23 15:10:45 +0900

    feat: 검색시 입력한 내용을 포함하는 이름을 가진 음식점을 페이지에 표시 #271

--- src/main/java/com/whiskey/rvcom/restaurant/controller/RestaurantViewController.java
+++ src/main/java/com/whiskey/rvcom/restaurant/controller/RestaurantViewController.java
@@ -1,6 +1,7 @@
 package com.whiskey.rvcom.restaurant.controller;
 
 import com.whiskey.rvcom.restaurant.dto.RestaurantCardDTO;
+import com.whiskey.rvcom.restaurant.dto.RestaurantSearchResultDTO;
 import com.whiskey.rvcom.restaurant.service.RestaurantService;
 import com.whiskey.rvcom.review.ReviewService;
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +13,6 @@ import java.util.List;
 
 @Controller
 @Slf4j
-@RequestMapping("/restaurant")
 public class RestaurantViewController {
 
     private final RestaurantService restaurantService;
@@ -21,16 +21,19 @@ public class RestaurantViewController {
         this.restaurantService = restaurantService;
     }
 
-    @GetMapping("/userLocationMain")
+    @GetMapping("/restaurant/userLocationMain")
     @ResponseBody
+    // main 페이지에서 사용자의 위치를 받아와 가까운 식당 정보를 반환
     public List<RestaurantCardDTO> userLocationMain(@RequestParam double lat, @RequestParam double lng) {
         return restaurantService.getNearbyRestaurantByLocation(lat, lng);
     }
 
     @GetMapping("/search-results")
     // 검색 결과 페이지로 이동
-    public String searchResults(@RequestParam(value = "searchText", required = false) String searchText, Model model) {
+    public String searchResults(@RequestParam(value = "search", required = false) String searchText, Model model) {
         model.addAttribute("searchText", searchText);
+        List<RestaurantSearchResultDTO> restaurantSearchResultList = restaurantService.getRestaurantsBySearchText(searchText);
+        model.addAttribute("restaurantSearchResult", restaurantSearchResultList);
         return "searchResults";
     }
 }
