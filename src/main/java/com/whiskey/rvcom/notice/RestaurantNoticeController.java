a19f3458cfe68b875fd01b13a050dc61b072459f
youngrok <yr951103@gmail.com>
2024-08-26 00:03:16 +0900

    feat: 음식점 공지사항 조회 사용자 페이지 분리
    - controller 분리
    - url 주소 변경

--- src/main/java/com/whiskey/rvcom/notice/RestaurantNoticeController.java
+++ src/main/java/com/whiskey/rvcom/notice/RestaurantNoticeController.java
@@ -5,15 +5,25 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 public class RestaurantNoticeController {
     private final RestaurantNoticeService noticeService;
+
+    /**
+     * 해당 음식점의 공지사항 전체 조회
+     * @param restaurantId
+     * @return 음식점 공지사항 리스트
+     */
+    @GetMapping("/restaurant/{restaurantId}/user-notice")
+    public List<RestaurantNoticeResponseDTO> findNoticeByRestaurantId(@PathVariable Long restaurantId) {
+        return noticeService.findNoticeByRestaurantId(restaurantId);
+    }
 
     /**
      * 해당 음식점의 공지사항 전체 조회
