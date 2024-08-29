171526878c61679eeb658b933c7d1fc047f49816
Jaehyeok Song <162393172+speter6501@users.noreply.github.com>
2024-08-29 12:37:13 +0900

    feat: 관리자페이지: 회원정보 조회 및 페이지네이션,
    -관리자페이지에서 회원정보 가져오기 및 페이지네이션 적용
    - 디자인 수정필요.
    -admincontroller에서 중복되는 내용 주석 및 관리자이름및 프로필 사진 로그아웃 구현중
    -이메일 인증 주소 변경

--- src/main/java/com/whiskey/rvcom/controller/AdminController.java
+++ src/main/java/com/whiskey/rvcom/controller/AdminController.java
@@ -1,5 +1,8 @@
 package com.whiskey.rvcom.controller;
 
+import com.whiskey.rvcom.entity.member.Member;
+import com.whiskey.rvcom.util.ImagePathParser;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +12,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
+//    @GetMapping("/adminMain")
+//    public String adminMain(Model model, HttpSession session) {
+//        Member member = (Member) session.getAttribute("member");
+//        if (member == null) {
+//            return "redirect:/login";
+//        }
+//
+//        String profileImageUrl = "https://i.kym-cdn.com/entries/icons/facebook/000/049/273/cover11.jpg";
+//        if (member.getProfileImage() != null) {
+//            profileImageUrl = ImagePathParser.parse(member.getProfileImage().getUuidFileName());
+//        }
+//
+//        model.addAttribute("profileImageUrl", profileImageUrl);
+//        model.addAttribute("memberName", member.getName());
+//        return "adminMain";
+//    }
+
     @GetMapping("/dashboard")
     public String getDashboard(Model model) {
         // 대시보드 데이터를 모델에 추가
         return "admin/fragments/dashboard :: dashboard";
     }
 
-    @GetMapping("/user-management")
-    public String getUserManagement(Model model) {
-        // 사용자 관리 데이터를 모델에 추가
-        return "admin/fragments/user-management :: userManagement";
-    }
+//    @GetMapping("/user-management")
+//    public String getUserManagement(Model model) {
+//        // 사용자 관리 데이터를 모델에 추가
+//        return "admin/fragments/user-management :: userManagement";
+//    }
 
     @GetMapping("/report-management")
     public String getReportManagement(Model model) {
